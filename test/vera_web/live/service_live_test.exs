9e094e7dc1d64fe84a7d1de211108ec4feac6357
Adrian Low <adrianlow1998@gmail.com>
2025-04-06 21:36:18 +0800

    updated service_live_test.exs

--- test/vera_web/live/service_live_test.exs
+++ test/vera_web/live/service_live_test.exs
@@ -7,26 +7,41 @@ defmodule VeraWeb.ServiceLiveTest do
   @create_attrs %{name: "some name"}
   @update_attrs %{name: "some updated name"}
   @invalid_attrs %{name: nil}
+  @create_child_attrs %{name: "child service"}
 
   defp create_service(_) do
     service = service_fixture()
     %{service: service}
   end
 
+  defp create_service_with_child(_) do
+    parent = service_fixture()
+    child = service_fixture(%{parent_id: parent.id})
+    %{parent: parent, child: child}
+  end
+
   describe "Index" do
     setup [:create_service]
 
-    test "lists all services", %{conn: conn, service: service} do
+    test "lists all root services", %{conn: conn, service: service} do
       {:ok, _index_live, html} = live(conn, ~p"/services")
 
-      assert html =~ "Listing Services"
+      assert html =~ "Services"
       assert html =~ service.name
+    end
+
+    test "doesn't list child services on index", %{conn: conn, service: parent} do
+      child = service_fixture(%{parent_id: parent.id})
+      {:ok, _index_live, html} = live(conn, ~p"/services")
+
+      assert html =~ parent.name
+      refute html =~ child.name
     end
 
     test "saves new service", %{conn: conn} do
       {:ok, index_live, _html} = live(conn, ~p"/services")
 
-      assert index_live |> element("a", "New Service") |> render_click() =~
+      assert index_live |> element("a", "Create Service") |> render_click() =~
                "New Service"
 
       assert_patch(index_live, ~p"/services/new")
@@ -35,14 +50,11 @@ defmodule VeraWeb.ServiceLiveTest do
              |> form("#service-form", service: @invalid_attrs)
              |> render_change() =~ "can&#39;t be blank"
 
-      assert index_live
+      html = index_live
              |> form("#service-form", service: @create_attrs)
              |> render_submit()
 
-      assert_patch(index_live, ~p"/services")
-
-      html = render(index_live)
-      assert html =~ "Service created successfully"
+      assert html =~ "Service created"
       assert html =~ "some name"
     end
 
@@ -58,14 +70,15 @@ defmodule VeraWeb.ServiceLiveTest do
              |> form("#service-form", service: @invalid_attrs)
              |> render_change() =~ "can&#39;t be blank"
 
-      assert index_live
-             |> form("#service-form", service: @update_attrs)
-             |> render_submit()
+      # First render the form submission
+      index_live
+      |> form("#service-form", service: @update_attrs)
+      |> render_submit()
 
-      assert_patch(index_live, ~p"/services")
-
+      # Then wait a brief moment and re-render to see the updated content
+      Process.sleep(100)
       html = render(index_live)
-      assert html =~ "Service updated successfully"
+      assert html =~ "Service updated"
       assert html =~ "some updated name"
     end
 
@@ -78,36 +91,111 @@ defmodule VeraWeb.ServiceLiveTest do
   end
 
   describe "Show" do
-    setup [:create_service]
+    setup [:create_service_with_child]
 
-    test "displays service", %{conn: conn, service: service} do
-      {:ok, _show_live, html} = live(conn, ~p"/services/#{service}")
+    test "displays service and its children", %{conn: conn, parent: parent, child: child} do
+      {:ok, _show_live, html} = live(conn, ~p"/services/#{parent}")
 
-      assert html =~ "Show Service"
-      assert html =~ service.name
+      assert html =~ parent.name
+      assert html =~ child.name
     end
 
-    test "updates service within modal", %{conn: conn, service: service} do
-      {:ok, show_live, _html} = live(conn, ~p"/services/#{service}")
+    test "creates new child service", %{conn: conn, parent: parent} do
+      {:ok, show_live, _html} = live(conn, ~p"/services/#{parent}")
 
-      assert show_live |> element("a", "Edit") |> render_click() =~
-               "Edit Service"
+      assert show_live |> element("a", "Create Service") |> render_click() =~
+               "New Service"
 
-      assert_patch(show_live, ~p"/services/#{service}/show/edit")
+      assert_patch(show_live, ~p"/services/#{parent}/new")
 
       assert show_live
              |> form("#service-form", service: @invalid_attrs)
              |> render_change() =~ "can&#39;t be blank"
 
+      html = show_live
+             |> form("#service-form", service: @create_child_attrs)
+             |> render_submit()
+
+      assert html =~ "Service created"
+      assert html =~ "child service"
+    end
+
+    test "updates child service", %{conn: conn, parent: parent, child: child} do
+      {:ok, show_live, _html} = live(conn, ~p"/services/#{parent}")
+
+      assert show_live |> element("#services-#{child.id} a", "Edit") |> render_click() =~
+               "Edit Service"
+
+      assert_patch(show_live, ~p"/services/#{parent}/edit/#{child.id}")
+
       assert show_live
+             |> form("#service-form", service: @invalid_attrs)
+             |> render_change() =~ "can&#39;t be blank"
+
+      html = show_live
              |> form("#service-form", service: @update_attrs)
              |> render_submit()
 
-      assert_patch(show_live, ~p"/services/#{service}")
+      assert html =~ "Service updated"
+      assert html =~ "some updated name"
+    end
+
+    test "deletes child service", %{conn: conn, parent: parent, child: child} do
+      {:ok, show_live, _html} = live(conn, ~p"/services/#{parent}")
+
+      assert show_live |> element("#services-#{child.id} a", "Delete") |> render_click()
+      refute has_element?(show_live, "#services-#{child.id}")
+    end
+
+    test "updates parent service", %{conn: conn, parent: parent} do
+      {:ok, _show_live, _html} = live(conn, ~p"/services/#{parent}")
+
+      # Navigate to edit page
+      {:ok, edit_live, _html} = live(conn, ~p"/services/#{parent}/edit")
+      assert render(edit_live) =~ "Edit Service"
+
+      # Submit the update form
+      html = edit_live
+             |> form("#service-form", service: @update_attrs)
+             |> render_submit()
+
+      # Wait briefly for the update to be applied
+      Process.sleep(100)
+
+      # Verify changes on parent's show page
+      {:ok, _show_live, html} = live(conn, ~p"/services/#{parent}")
+
+      # Get the updated parent from the database to ensure it matches
+      updated_parent = Vera.Services.get_service!(parent.id)
+      assert html =~ updated_parent.name
+      assert updated_parent.name == @update_attrs.name
+    end
+
+    test "handles pub/sub service updates", %{conn: conn, parent: parent, child: child} do
+      {:ok, show_live, _html} = live(conn, ~p"/services/#{parent}")
+
+      send(show_live.pid, {:service_updated, %{child | name: "updated via pubsub"}})
 
       html = render(show_live)
-      assert html =~ "Service updated successfully"
-      assert html =~ "some updated name"
+      assert html =~ "updated via pubsub"
+    end
+
+    test "handles service deletion with redirect", %{conn: conn, parent: parent} do
+      {:ok, show_live, _html} = live(conn, ~p"/services/#{parent}")
+
+      send(show_live.pid, {:service_deleted, parent, nil})
+
+      assert_redirect(show_live, ~p"/services")
+    end
+
+    test "shows connected clients count", %{conn: conn, parent: parent} do
+      {:ok, show_live, html} = live(conn, ~p"/services/#{parent}")
+
+      assert html =~ "0 client connected"
+
+      send(show_live.pid, {:clients_connected, 2})
+      html = render(show_live)
+      assert html =~ "2 clients connected"
     end
   end
 end
