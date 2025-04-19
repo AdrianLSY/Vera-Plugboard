8f0b14ee33ca88d7f1469b330df6698f0e1f1611
Adrian Low <adrianlow1998@gmail.com>
2025-04-20 00:39:50 +0800

    small fixes

--- priv/repo/migrations/20250418170554_create_services_tokens.exs
+++ priv/repo/migrations/20250418170554_create_services_tokens.exs
@@ -3,9 +3,9 @@ defmodule Vera.Repo.Migrations.CreateServicesTokens do
 
   def change do
     create table(:services_tokens) do
+      add :service_id, references(:services, on_delete: :delete_all), null: false
       add :token, :binary, null: false
       add :context, :string, null: false
-      add :service_id, references(:services, on_delete: :delete_all), null: false
 
       timestamps(type: :utc_datetime, updated_at: false)
     end
